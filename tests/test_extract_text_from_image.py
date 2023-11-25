9fe2cc11077821e1a550bf254b4f3498d878555d
y-tsuritani <ykt.tsuriya@gmail.com>
2023-11-26 01:49:16 +0900

    add: テストケースの追加

--- tests/test_extract_text_from_image.py
+++ tests/test_extract_text_from_image.py
@@ -20,10 +20,10 @@ def mock_vision_client(mocker):
     return mock_client
 
 
-def test_extract_text_from_image(mock_vision_client):
+def test_extract_text_from_image(mock_vision_client, mocker):
     # モックオブジェクトの設定
     mock_blob = MockBlob(content=b"test content")
-    mock_response = Mock()
+    mock_response = mocker.Mock()
     mock_response.full_text_annotation.text = "extracted text"
     mock_vision_client.document_text_detection.return_value = mock_response
 
@@ -34,6 +34,50 @@ def test_extract_text_from_image(mock_vision_client):
     assert result == "extracted text"
 
     # 関数が期待通りに呼び出されたことを確認
+    mock_vision_client.document_text_detection.assert_called_once_with(
+        image=types.Image(content=mock_blob.download_as_bytes())
+    )
+
+
+# OCRがテキストを検出できなかった場合のテスト
+def test_extract_text_from_empty_image(mock_vision_client, mocker):
+    mock_blob = MockBlob(content=b"")
+    mock_response = mocker.Mock()
+    mock_response.full_text_annotation.text = ""
+    mock_vision_client.document_text_detection.return_value = mock_response
+
+    result = extract_text_from_image(mock_blob)
+    # 空のテキストを返す場合
+    assert result == ""
+
+    mock_vision_client.document_text_detection.assert_called_once_with(
+        image=types.Image(content=mock_blob.download_as_bytes())
+    )
+
+
+# OCR処理中に例外が発生した場合のテスト
+def test_extract_text_with_error(mock_vision_client, mocker):
+    mock_blob = MockBlob(content=b"test content")
+    mock_vision_client.document_text_detection.side_effect = Exception("Error")
+
+    with pytest.raises(Exception) as e:
+        extract_text_from_image(mock_blob)
+    assert str(e.value) == "Error"
+
+    mock_vision_client.document_text_detection.assert_called_once_with(
+        image=types.Image(content=mock_blob.download_as_bytes())
+    )
+
+
+def test_extract_text_from_different_content_type(mock_vision_client, mocker):
+    mock_blob = MockBlob(content=b"different content")
+    mock_response = mocker.Mock()
+    mock_response.full_text_annotation.text = "different extracted text"
+    mock_vision_client.document_text_detection.return_value = mock_response
+
+    result = extract_text_from_image(mock_blob)
+    assert result == "different extracted text"
+
     mock_vision_client.document_text_detection.assert_called_once_with(
         image=types.Image(content=mock_blob.download_as_bytes())
     )

390a24424a8bfa8cd80aa3c2614e78b352e20920
y-tsuritani <ykt.tsuriya@gmail.com>
2023-11-26 01:49:02 +0900

    add: テストケースの追加

--- tests/test_list_blobs_in_order.py
+++ tests/test_list_blobs_in_order.py
@@ -23,3 +23,26 @@ def test_list_blobs_in_order(mock_client):
     assert list_blobs_in_order(bucket_name) == sorted(
         expected_files
     ), "The blob names should be returned in sorted order"
+
+
+def test_list_blobs_in_order_raises_exception(mock_client, mocker):
+    # モックオブジェクトの設定
+    mock_client.list_blobs.side_effect = Exception("Error")
+
+    # テスト対象の関数を呼び出すと例外が発生することを確認
+    with pytest.raises(Exception) as e:
+        list_blobs_in_order("test-bucket")
+    assert str(e.value) == "Error"
+
+    # 関数が期待通りに呼び出されたことを確認
+    mock_client.list_blobs.assert_called_once_with("test-bucket")
+
+
+def test_list_blobs_in_order_with_empty_bucket(mock_client):
+    mock_client.list_blobs.return_value = []
+
+    result = list_blobs_in_order("empty-bucket")
+
+    assert result == [], "The result should be an empty list for an empty bucket"
+
+    mock_client.list_blobs.assert_called_once_with("empty-bucket")
